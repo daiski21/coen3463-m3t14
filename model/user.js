689f07beac9dc4f92718e55f392a63b68d9b5777
daiski21 <ampitmelving@gmail.com>
2017-01-28 20:40:08 +0800

    login

--- model/user.js
+++ model/user.js
@@ -2,8 +2,18 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     passportLocalMongoose = require('passport-local-mongoose');
 
-var User = new Schema({
-});
+var User = new Schema
+(
+	{
+		fname: String,
+		lname: String,
+		username: String,
+		email: String,
+	},
+	//{
+//		collection: 'User'
+//	}
+);
 
 User.plugin(passportLocalMongoose);
 
