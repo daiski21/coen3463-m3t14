64f113be2bb94a9912cb445b50bfc55f5ac07a3e
daiski21 <ampitmelving@gmail.com>
2017-01-31 12:49:40 +0800

    index updated

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-  	user: req.user,
-  	title: 'Express' 
-  });
+  if (req.user){
+ 	res.render('index', { 
+ 			user: req.user,
+ 		 	title: 'Express' 
+ 		});
+ 	}
+ 	else{
+ 		res.redirect('/auth/login');
+ 	}
 });
 
 module.exports = router;
