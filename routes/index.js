955d11c9ebe4e0ceb5ee93c0222ea3621f303f44
daiski21 <ampitmelving@gmail.com>
2017-02-01 14:24:48 +0800

    new update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
   if (req.user){
  	res.render('index', { 
  			user: req.user,
@@ -13,17 +12,6 @@ router.get('/', function(req, res, next) {
  	else{
  		res.redirect('/auth/login');
  	}
-=======
-	if (req.user){
-		res.render('index', { 
-			user: req.user,
-		 	title: 'Express' 
-		});
-	}
-	else{
-		res.redirect('/auth/login');
-	}
->>>>>>> 011d97f9edaafd2a92dc4bcfcc044dbd3258ab89
 });
 
 module.exports = router;
