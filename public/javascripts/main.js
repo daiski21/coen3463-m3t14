f892e0f47df245c829a3355ce7d4a8754ef0918f
daiski21 <ampitmelving@gmail.com>
2017-02-06 00:55:14 +0800

    SearchBar Updated

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,37 +16,68 @@ $('#delete').on('click', function(e){
   });
 });
 
-
+function getSearch() {
+	localStorage.setItem("search", document.getElementById('search').value);
+}
+ 
 
 if (window.location.pathname === '/blobs/') {
 
-  fetch('../api/v1/workouts?sort=date').then(function(res) {
-    res.json().then(function(workouts) {	
-      console.log('workouts', workouts);
-      var tbody = document.getElementById('table-body');
-      workouts.forEach(function(workouts) {
-        tbody.insertAdjacentHTML('beforeend', '<tr><td><a href="/blobs/' + workouts._id + '">'
-         + workouts.name + '</td><td>' + workouts.MMG + ' </td><td>' 
-         + workouts.DMG + ' </td><td>' + workouts.OMG + ' </td><td>' 
-         + workouts.type + ' </td><td>' + workouts.mechanics + ' </td><td>' 
-         + workouts.equipment + ' </td><td>' + workouts.difficulty + ' </td><td>' 
-         + workouts.date + ' </td><td><a href="/blobs/' 
-         + workouts._id + '/edit">' + 'Edit' + '</td><td><form action= "/blobs/' + workouts._id + '/edit" method="post" enctype="application/x-www-form-urlencoded"><input type="hidden" value="DELETE" name="_method"><button type="submit" style="width:55px;height:25px;" position= "center"> Delete</form></td></tr>' );
+	if (localStorage.getItem("search") === 'null') {
 
-      });
-    })
-  });
+		  fetch('../api/v1/workouts?sort=date').then(function(res) {
+		    res.json().then(function(workouts) {	
+		      console.log('workouts', workouts);
+		      var tbody = document.getElementById('table-body');
+		      workouts.forEach(function(workouts) {
+		        tbody.insertAdjacentHTML('beforeend', '<tr><td><a href="/blobs/' + workouts._id + '">'
+		         + workouts.name + '</td><td>' + workouts.MMG + ' </td><td>' 
+		         + workouts.DMG + ' </td><td>' + workouts.OMG + ' </td><td>' 
+		         + workouts.type + ' </td><td>' + workouts.mechanics + ' </td><td>' 
+		         + workouts.equipment + ' </td><td>' + workouts.difficulty + ' </td><td>' 
+		         + workouts.date + ' </td><td><a href="/blobs/' 
+		         + workouts._id + '/edit">' + 'Edit' + '</td><td><form action= "/blobs/' + workouts._id + '/edit" method="post" enctype="application/x-www-form-urlencoded"><input type="hidden" value="DELETE" name="_method"><button type="submit" style="width:55px;height:25px;" position= "center"> Delete</form></td></tr>' );
 
-  fetch('../api/v1/workouts/count').then(function(res){
-		res.json().then(function(count){
-			console.log('count', count)
-			var totalCount = document.getElementById('totalCount');
-			setTimeout(function() {
-				totalCount.innerHTML = count.count;
-			}, 500)
-			
+		      });
+		    })
+		  });
+
+		  fetch('../api/v1/workouts/count').then(function(res){
+				res.json().then(function(count){
+					console.log('count', count)
+					var totalCount = document.getElementById('totalCount');
+					setTimeout(function() {
+						totalCount.innerHTML = count.count;
+					}, 500)
+					
+				});
+			});
+	}	  
+	else {
+		fetch('../api/v1/workouts?query={"name":"~(' + localStorage.getItem("search") + ')"}').then(function(res) {
+			res.json().then(function(result) {
+				if (result.length === 1) {
+					document.getElementById('totalCount').innerHTML = "Found " + result.length +
+				" entry ";
+				}
+				else {
+					document.getElementById('totalCount').innerHTML = "Found " + result.length +
+				" entries ";
+				}
+				
+				var tbody = document.getElementById('table-body');
+				result.forEach(function(result) {
+				 tbody.insertAdjacentHTML('beforeend', '<tr><td><a href="/blobs/' + result._id + '">'
+			         + result.name + '</td><td>' + result.MMG + ' </td><td>' 
+			         + result.DMG + ' </td><td>' + result.OMG + ' </td><td>' 
+			         + result.type + ' </td><td>' + result.mechanics + ' </td><td>' 
+			         + result.equipment + ' </td><td>' + result.difficulty + ' </td><td>' 
+			         + result.date + ' </td><td><a href="/blobs/' 
+			         + result._id + '/edit">' + 'Edit' + '</td><td><form action= "/blobs/' + result._id + '/edit" method="post" enctype="application/x-www-form-urlencoded"><input type="hidden" value="DELETE" name="_method"><button type="submit" style="width:55px;height:25px;" position= "center"> Delete</form></td></tr>' );
+
+				});
+				localStorage.setItem("search", null);
+			});
 		});
-	});
-
-
+	}
 }
